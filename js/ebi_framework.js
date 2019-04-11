8cbbedfe42fc8ebf89de91585e742c2ae11bc9a3
Maged Eladawy <maged@ebi.ac.uk>
2019-04-11 16:17:27 +0100

    Add responsive menu

--- js/ebi_framework.js
+++ js/ebi_framework.js
@@ -12,6 +12,12 @@
     attach: function (context, settings) {
       //alert("I'm alive!");
       $(document).foundationExtendEBI();
+      // Add active class for parent dropdown menu.
+      $('ul.menu.dropdown li.is-dropdown-submenu-parent a.is-active').parents('li.is-dropdown-submenu-parent').find('> a').addClass('is-active');
+      $('.responsive-menu-expand').once().click(function() {
+        console.log('asdadsad');
+        $('.menu-wrapper').toggleClass('hide-for-small-only');
+      });
     }
   };
 
