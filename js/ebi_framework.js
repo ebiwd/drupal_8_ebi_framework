4d7eac7badd8c943901a1096b1486a3fb03dbd4e
Maged Eladawy <maged@ebi.ac.uk>
2019-04-11 16:17:55 +0100

    Remove debugging code

--- js/ebi_framework.js
+++ js/ebi_framework.js
@@ -15,7 +15,6 @@
       // Add active class for parent dropdown menu.
       $('ul.menu.dropdown li.is-dropdown-submenu-parent a.is-active').parents('li.is-dropdown-submenu-parent').find('> a').addClass('is-active');
       $('.responsive-menu-expand').once().click(function() {
-        console.log('asdadsad');
         $('.menu-wrapper').toggleClass('hide-for-small-only');
       });
     }
